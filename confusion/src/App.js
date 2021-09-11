9777535727476c78f41977028cdf6accfcd48912
xeni85 <shkelzen.dunisha@umsh.edu.al>
2021-09-11 19:05:26 +0200

    Component part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,30 @@
+import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { Component } from 'react';
 import './App.css';
+import { DISHES } from './shared/dishes'
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand>Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        dishes: DISHES
+      };
+    };
+    render() {
+      
+      return (
+        <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand>Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
         </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
-}
+      );
+    }
+  };
 
-export default App;
+    export default App;
